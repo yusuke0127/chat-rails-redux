d38acde72885e7372db5bf3009e1610d4bda769c
yusuke ishida <ishidayusuke01@gmail.com>
2020-07-18 17:09:21 +0900

    Created models and seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,38 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+
+puts "Running seed file"
+puts "Clearing databse..."
+
+Message.destroy_all
+User.destroy_all
+Channel.destroy_all
+
+channels = ["general", "react", "tokyo"]
+
+channels.each do |channel|
+  @channel = Channel.create!(name: channel)
+  puts "Done creating #{@channel.name}"
+end
+
+puts "Done creating channels"
+
+
+@yusuke = User.create(
+  email: "yusuke@gmail.com",
+  password: "123456"
+)
+
+puts "Done creating yusuke"
+
+10.times  do
+  @message = Message.new(content: Faker::Movie.quote)
+  @message.channel = Channel.first
+  @message.user = @yusuke
+  @message.save!
+end
+
+puts "Done creating messages"
