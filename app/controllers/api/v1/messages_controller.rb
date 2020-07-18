9b8f0c9bf7f7018acc53ad1bf23873c65be61d5b
yusuke ishida <ishidayusuke01@gmail.com>
2020-07-18 18:19:46 +0900

    Converting instances to json

--- app/controllers/api/v1/messages_controller.rb
+++ app/controllers/api/v1/messages_controller.rb
@@ -2,7 +2,9 @@ class Api::V1::MessagesController < ApplicationController
   def index
     @channel = Channel.find_by name: params[:channel_id]
     @messages = @channel.messages
-    render json: @messages
+    render json: @messages.as_json(
+      only: [:id, :content, :created_at, :user_id],
+    )
   end
 
   def create
